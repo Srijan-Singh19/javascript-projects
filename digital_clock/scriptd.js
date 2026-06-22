9da0aaa4508c146dd4f177930891fe6a902aee19
SRIJAN SINGH <neetusrijan1212@gmail.com>
2026-06-22 11:49:49 +0530

    Added the AM/PM and 12 formate

--- digital_clock/scriptd.js
+++ digital_clock/scriptd.js
@@ -1,19 +1,44 @@
 const clock = document.getElementById("clock");
+const dates = document.getElementById("date");
 
-function updateclock(){
+function updateClock() {
     const now = new Date();
 
     let hour = now.getHours();
     let min = now.getMinutes();
     let sec = now.getSeconds();
 
-    hour = String(hour).padStart(2,"0");
-    min = String(min).padStart(2,"0");
-    sec = String(sec).padStart(2,"0");
+    hour = String(hour).padStart(2, "0");
+    min = String(min).padStart(2, "0");
+    sec = String(sec).padStart(2, "0");
 
-    clock.textContent= `${hour}:${min}:${sec}`;
+    let period= hour>=12?"PM":"AM";
+
+    hour=hour%12;
+    hour=hour||12;
+
+    clock.textContent = `${hour}:${min}:${sec} ${period}`;
+
 }
 
-updateclock();
+const months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+];
 
-setInterval(updateclock,1000);
+function updateDate() {
+    const date = new Date();
+
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    dates.textContent = `${day} ${month} ${year}`;
+}
+
+updateDate();
+updateClock();
+
+setInterval(updateClock, 1000);
+setInterval(updateDate, 60000);
